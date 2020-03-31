e78d9c6ccd08f0ac58d64758c61fada32b898310
Carlos G. Gavidia <cgavidiac@gmail.com>
2020-03-31 09:04:45 +0100

    Improvements for plots

--- equilibrium_plots/equilibrium_plots.R
+++ equilibrium_plots/equilibrium_plots.R
@@ -3,10 +3,15 @@ library(tidyverse)
 data_file <- read_csv("equilibria.csv")
 all_equilibria <- data_file %>% 
   mutate(Parameters = replace_na(Parameters, ""))  %>% 
-  mutate(Configuration = str_c(Process, Parameters, sep = " "))
+  mutate(ProcessCode = recode(Process, `Distributed Prioritisation` = "DP", Gatekeeper = "GK",
+                              `Assessor-Throttling` = "AT")) %>%
+  mutate(Configuration = str_c(ProcessCode, Parameters, EquilibriumId, sep = " ")) 
 
 reduced_bandwith <- filter(all_equilibria, Scenario == "Reduced Bandwidth")
+full_bandwidth <- filter(all_equilibria, Scenario == "Full Bandwidth")
 
-ggplot(data = reduced_bandwith) + 
+data_for_plot <- full_bandwidth
+bar_position <- "fill"
+ggplot(data = data_for_plot) + 
   geom_bar(mapping = aes(x = Configuration, y = Probability, fill = Strategy),
-           position = "dodge", stat = "identity")
+           position = bar_position, stat = "identity")
